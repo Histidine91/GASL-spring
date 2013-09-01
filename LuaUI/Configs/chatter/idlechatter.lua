6bd641c67c42ead220a1af14d0770b1c3913356d
L.J. Lim <kingraptorowns@gmail.com>
2013-09-01 20:02:36 +0800

    No idle chatter outside of missions until everyone has > 1 line

--- LuaUI/Configs/chatter/idlechatter.lua
+++ LuaUI/Configs/chatter/idlechatter.lua
@@ -1,17 +1,19 @@
 -- separate file so missions don't need to make a copy of the entire pilotdefs
 local idleChatterDefs = {
+	--[[
 	luckystar = {
-		--{image = portraits.milfeulle_happy, text = "Tact! Are you looking at me?"},
+		{image = portraits.milfeulle_happy, text = "Tact! Are you looking at me?"},
 	},
 	kungfufighter = {
-		--{image = portraits.ranpha_normal, text = "Ah, Tact. Checking out my sweet moves?"},
+		{image = portraits.ranpha_normal, text = "Ah, Tact. Checking out my sweet moves?"},
 	},
 	trickmaster = {},
 	happytrigger = {
-		--{image = portraits.forte_what, text = "Tact, don't you have better things to do than look over my shoulder?"},
+		{image = portraits.forte_what, text = "Tact, don't you have better things to do than look over my shoulder?"},
 	},
 	harvester = {},
-	sharpshooter = {},	
+	sharpshooter = {},
+	]]
 }
 
 return idleChatterDefs
